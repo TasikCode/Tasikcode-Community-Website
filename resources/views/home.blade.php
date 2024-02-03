0a8514be047cd69e0c6ee3c8ffdb79d8b356d118
Debian <bitnami@ip-172-26-4-191.ap-northeast-2.compute.internal>
2024-02-03 04:35:57 +0000

    Off we go!

---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+Tasikcode 2024 Hackathon - Building Tasikcode Community Webiste<br/>
+visit : <a href="https://github.com/TasikCode/Tasikcode-Community-Website">https://github.com/TasikCode/Tasikcode-Community-Website</a><br />
+For more detail about the project
